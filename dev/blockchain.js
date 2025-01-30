56a29ec234a16ac360069cc791768b8ca4c9a36a
Alfonso Zazueta <92172409+alfonsozazueta@users.noreply.github.com>
2025-01-29 22:02:13 -0600

    Blockchain V2
    This version of the blockchain js is tested through an API that I have develope to test features like mining, pending transactions and adding new transactions to the blockchain

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,5 +1,7 @@
 const sha256 = require('sha256')
+//Requerimos sha256 protocolo hashing
 
+//Inicializamos la cadena y transacciones pendientes, creamos el bloque genesis con standar values
 function Blockchain(){
     this.chain = []
     this.pendingTransactions = []
@@ -7,6 +9,9 @@ function Blockchain(){
     this.createNewBlock(100, '0', '0')
 }
 
+//Este método crea un bloque nuevo, con un nonce que satisface la prueba de trabajo
+//previousBlockHash hash del bloque anterior
+//hash hash generado en nuevo bloque
 Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash,hash){
     const newBlock={
         index: this.chain.length + 1,
@@ -23,10 +28,13 @@ Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash,hash){
     return newBlock
 }
 
+//Este método devuelve el ùltimo bloque de la cadena
 Blockchain.prototype.getLastBlock = function(){
     return this.chain[this.chain.length - 1];
 };
 
+//Método que crea una nueva transaccion y la almacena en Pedning
+//Devuelve el index del próximo bloque donde se alacenará la transacción
 Blockchain.prototype.createNewTransaction = function(amount, sender, receiver){
     const newTransaction = {
         amount: amount,
@@ -38,12 +46,16 @@ Blockchain.prototype.createNewTransaction = function(amount, sender, receiver){
     return this.getLastBlock()['index'] + 1;
 };
 
+//Toma el previoushash, los datos del current block y el nonce
+//Concatena todo un str y genera hash usando librebria sha256
 Blockchain.prototype.hashBlock = function(previousBlockHash, currentBlockData, nonce){
     const dataAsString = previousBlockHash + String(nonce) + JSON.stringify(currentBlockData)
     const hash = sha256(dataAsString)
     return hash
 }
 
+//PoW para encontrar un hash que empiece con '0000'
+//Incrementa el nonce hasta encontrar un hash que sea válido
 Blockchain.prototype.proofOfWoork = function(previousBlockHash, currentBlockData) {
     let nonce = 0
     //let because nonce and hash will be mutating
